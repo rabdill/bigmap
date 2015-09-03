3ec436f0e00f94ca309a3782dbc7834b4307f2b1
Rich Abdill <richard.abdill@gmail.com>
2015-09-03 17:47:02 -0400

    Started adding filters for region lists

--- frontend/app/controllers.js
+++ frontend/app/controllers.js
@@ -9,19 +9,30 @@ bigmapCtrl.controller('FrameCtrl', function ($scope, $http) {
 		console.log(data);
   	$scope.regions = data.regions;
 		$scope.player = data.player;
-
-
-
-		$scope.newGame = function() {
-			$http.post('http://localhost:3000/regions/init').success(function(data) {
-				$scope.message = data.message;
-				console.log(data);
-			});
-		}
 	});
+
+	$scope.newGame = function() {
+		$http.post('http://localhost:3000/regions/init').success(function(data) {
+			$scope.message = data.message;
+			console.log(data);
+		});
+	}
+
+	// for filtering the region lists:
+	$scope.playerRegions = function(value) {
+		return value.control === $scope.player;
+	}
+
+	$scope.enemyRegions = function(value) {
+		return value.control !== $scope.player;
+	}
 });
 
 // Lists the currently allowed actions:
-bigmapCtrl.controller('OptionsCtrl', function ($scope) {
+bigmapCtrl.controller('OptionsCtrl', function ($scope, $http) {
+	// get the gamedata:
+	$http.get('http://localhost:3000/regions').success(function(data) {
+	});
+
 	$scope.choices = ['choice1', 'choice2'];
 });
